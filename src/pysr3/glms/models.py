6321bd2848756cf57e152516705f68d8d6b772a9
Aleksei Sholokhov <aksh@uw.edu>
2021-11-11 16:27:04 -0800

    Add PoissonSCAD; Remove positive regularizer from GLMs

--- src/pysr3/glms/models.py
+++ src/pysr3/glms/models.py
@@ -5,7 +5,7 @@ import numpy as np
 from pysr3.linear.models import SimpleLinearModel, SimpleLinearModelSR3
 from pysr3.linear.problems import LinearProblem
 from pysr3.glms.problems import PoissonProblem
-from pysr3.regularizers import DummyRegularizer, L1Regularizer, PositiveQuadrantRegularizer
+from pysr3.regularizers import DummyRegularizer, L1Regularizer, PositiveQuadrantRegularizer, SCADRegularizer
 from pysr3.solvers import PGDSolver
 from pysr3.glms.oracles import GLMOracle, GLMOracleSR3
 from pysr3.glms.link_functions import PoissonLinkFunction
@@ -18,7 +18,6 @@ class SimplePoissonModel(SimpleLinearModel):
         oracle, regularizer, solver = super().instantiate()
         link_function = PoissonLinkFunction()
         oracle = GLMOracle(problem=None, prior=self.prior, link_function=link_function)
-        regularizer = PositiveQuadrantRegularizer()
         return oracle, regularizer, solver
 
     def get_information_criterion(self, x, y, ic="bic"):
@@ -86,7 +85,6 @@ class SimplePoissonModelSR3(SimpleLinearModelSR3):
         oracle, regularizer, solver = super().instantiate()
         link_function = PoissonLinkFunction()
         oracle = GLMOracleSR3(problem=None, prior=self.prior, lam=self.el, link_function=link_function, constraints=self.constraints)
-        regularizer = PositiveQuadrantRegularizer()
         return oracle, regularizer, solver
 
     def _get_tags(self):
@@ -188,8 +186,7 @@ class PoissonL1Model(SimplePoissonModel):
 
     def instantiate(self):
         oracle, regularizer, solver = super().instantiate()
-        lasso = L1Regularizer(lam=self.alpha)
-        regularizer = PositiveQuadrantRegularizer(other_regularizer=lasso)
+        regularizer = L1Regularizer(lam=self.alpha)
         return oracle, regularizer, solver
 
 
@@ -247,6 +244,68 @@ class PoissonL1ModelSR3(SimplePoissonModelSR3):
 
     def instantiate(self):
         oracle, regularizer, solver = super().instantiate()
-        lasso = L1Regularizer(lam=self.alpha)
-        regularizer = PositiveQuadrantRegularizer(other_regularizer=lasso)
+        regularizer = L1Regularizer(lam=self.alpha)
+        return oracle, regularizer, solver
+
+
+
+class PoissonSCADModelSR3(SimplePoissonModelSR3):
+    def __init__(self,
+                 alpha: float = 0.,
+                 rho: float = 3.7,
+                 sigma: float = 1.,
+                 el: float = 1.,
+                 tol_solver: float = 1e-5,
+                 max_iter_solver: int = 1000,
+                 stepping: str = "fixed",
+                 logger_keys: Set = ('converged',),
+                 fixed_step_len=None,
+                 prior=None,
+                 practical=False,
+                 constraints=None,
+                 **kwargs):
+        """
+        Initializes the model
+
+        Parameters
+        ----------
+        alpha: float
+            strength of LASSO regularizer
+        el: float
+            constant for SR3 relaxation. Bigger values correspond to tighter relaxation.
+        tol_solver: float
+            tolerance for the stop criterion of PGD solver
+        max_iter_solver: int
+            maximal number of iterations for PGD solver
+        stepping: str
+            step-size policy for PGD. Can be either "line-search" or "fixed"
+        logger_keys: List[str]
+            list of keys for the parameters that the logger should track
+        fixed_step_len: float
+            step-size for PGD algorithm. If "linear-search" is used for stepping
+            then the algorithm uses this value as the maximal step possible. Use
+            this parameter if you know the Lipschitz-smoothness constant L for your problem
+            as fixed_step_len=1/L.
+        prior: Optional[Prior]
+            an instance of Prior class. If None then a non-informative prior is used.
+        kwargs:
+            for passing debugging info
+        """
+        super().__init__(el=el,
+                         tol_solver=tol_solver,
+                         max_iter_solver=max_iter_solver,
+                         stepping=stepping,
+                         logger_keys=logger_keys,
+                         fixed_step_len=fixed_step_len,
+                         prior=prior,
+                         practical=practical,
+                         constraints=constraints,
+                         **kwargs)
+        self.alpha = alpha
+        self.rho = rho
+        self.sigma = sigma
+
+    def instantiate(self):
+        oracle, regularizer, solver = super().instantiate()
+        regularizer = SCADRegularizer(lam=self.alpha, rho=self.rho, sigma=self.sigma)
         return oracle, regularizer, solver
